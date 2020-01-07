639df597955efe01762389a79c930f103a0380a7
Volodymyr Yevdokymov <vevdokimov@geeksforless.net>
2020-01-07 23:20:28 +0200

    base

---
+++ Podfile
@@ -0,0 +1,6 @@
+source 'https://github.com/thomasRim/Specs.git'
+source 'https://github.com/CocoaPods/Specs.git'
+
+target 'RRStructureScanner5' do
+	pod 'StructureScanner', '0.11.1'
+end
---
+++ RRStructureScanner/SupportFiles/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  RRStructureScanner5
+//
+//  Created by Vladimir Yevdokimov on 07.01.2020.
+//  Copyright © 2020 Ruthless Research, LLC. All rights reserved.
+//
+
+import UIKit
+import StructureScanner
+
+class MainViewController: UIViewController {
+
+    @IBAction fileprivate func startScan(WithSender sender: AnyObject?) {
+        let vc = ViewController();
+        
+    }
+}
